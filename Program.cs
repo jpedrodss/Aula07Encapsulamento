7614898a14ab4a80c7f6a804c7c0f5ff275aa7d1
João Pedro <jpedrodss@gmail.com>
2020-06-01 22:21:35 -0300

    projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula07Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
